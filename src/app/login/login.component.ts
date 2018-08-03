bb4c105ff7a6011b36abb7073b24d9e74b0c90a0
coolxjay <coolxjay@gmail.com>
2018-08-03 11:26:58 +0900

    After remember

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -20,9 +20,20 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+  	var credentials = JSON.parse(localStorage.getItem("remember"));
+  	if( credentials ) {
+  		this.user.username = credentials.username;
+  		this.user.password = credentials.password;
+  	}
   }
 
   onSubmit() {
+
+  	if( this.user.remember ) {
+  		localStorage.setItem("remember", JSON.stringify({username: this.user.username, password: this.user.password}));
+  	} else {
+  		localStorage.removeItem("remember");
+  	}
 
     console.log("User: ", this.user);
 		this.authService.logIn(this.user)
